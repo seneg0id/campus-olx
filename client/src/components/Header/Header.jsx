52e5b2bd8a73402b3f507fe257466138595f9755
seneg0id <vikas.kapuluru@gmail.com>
2022-07-02 11:42:22 +0530

    changed

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,48 +1,53 @@
-import React from 'react'
-import './header.css'
-import SearchIcon from '@mui/icons-material/Search'
-import logo from '../../images/logo.png'
+import React from "react";
+import "./header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import logo from "../../images/logo.png";
 
 const Header = () => {
   const nav = [
-    {ID:1,label:"Mobile Phones"},
-    {ID:2,label:"bicycles"},
-    {ID:3,label:"Laptops"},
-    {ID:4,label:"Computer & Accessories"},
-    {ID:5,label:"Cameras"}
-  ]
+    { ID: 1, label: "Mobile Phones" },
+    { ID: 2, label: "bicycles" },
+    { ID: 3, label: "Laptops" },
+    { ID: 4, label: "Computer & Accessories" },
+    { ID: 5, label: "Cameras" },
+  ];
   return (
-    <div className='hnav'>
-    <div className='Header-container'>
-      <div className="Header-leftContainer">
-        <img src={logo} alt="Olx-Clone-Logo" className='Header-leftContainerLogo'/>
+    <div className="hnav">
+      <div className="Header-container">
+        <div className="Header-leftContainer">
+          <img
+            src={logo}
+            alt="Olx-Clone-Logo"
+            className="Header-leftContainerLogo"
+          />
+        </div>
+        <div className="Header-centerContainer">
+          <input
+            type="text"
+            placeholder="Search"
+            className="Header-centerInputField"
+          />
+          <SearchIcon className="Header-centerIcon" />
+        </div>
+        <div className="Header-rightContainer">
+          <h4 className="Header-rightLinkText">Login</h4>
+          <button className="Header-rightButton">SELL</button>
+        </div>
       </div>
-      <div className="Header-centerContainer">
-        <input type="text" placeholder='Search' className='Header-centerInputField'/>
-        <SearchIcon className='Header-centerIcon'/>
-      </div>
-      <div className="Header-rightContainer">
-        <h4 className='Header-rightLinkText'>Login</h4>
-        <button className='Header-rightButton'>SELL</button>
-      </div>
-    </div>
-    <div className="Categories-available">
+      <div className="Categories-available">
         <button className="Category-title">
           <h4>All Categories</h4>
-          <i class="fa-solid fa-angle-down"></i>
+          <ExpandMoreIcon />
         </button>
-        <div className='Actual-categories'>
-          {
-            nav.map(item=>{
-              return(
-                <h4 className='Categories-inStock'>{item.label}</h4>
-              )
-            })
-          }
+        <div className="Actual-categories">
+          {nav.map((item) => {
+            return <h4 className="Categories-inStock">{item.label}</h4>;
+          })}
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
