0355e7499e7c268ee2dd2caa10a46f57a8ed4656
seneg0id <vikas.kapuluru@gmail.com>
2022-07-02 17:29:35 +0530

    changes

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -2,42 +2,31 @@ import { FavoriteBorder } from "@mui/icons-material";
 import styled from "styled-components";
 
 const Container = styled.div`
-  flex  :1;
-  margin :5px;
-  min-width:280px;
-  height:380px;
-  background-color:white;
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 380px;
+  background-color: white;
 `;
 
-const Text = styled.p`
+const Text = styled.p``;
+const Image = styled.img``;
+const Icon = styled.div``;
+const Info = styled.div``;
 
-`
-const Image = styled.img`
-
-`
-const Icon = styled.div`
-
-`
-const Circle = styled.div`
-
-`
-const Info = styled .div`
-`
-
-const Product = ({item}) => {
+const Product = ({ item }) => {
   return (
     <Container>
-        <Circle />
-        <Image src =  {item.image}/>
-        <Info>
-            <Icon>
-                <FavoriteBorder />
-            </Icon>
-            <Text>{item.title}</Text>
-            <Text>{item.price}</Text>
-        </Info>
+      <Image src={item.image} />
+      <Info>
+        <Icon>
+          <FavoriteBorder />
+        </Icon>
+        <Text>{item.title}</Text>
+        <Text>{item.price}</Text>
+      </Info>
     </Container>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
